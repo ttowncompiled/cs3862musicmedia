bdccbb0139014496abc8ded9de30d59c5dd7ef08
Connor <guardianx515@gmail.com>
2016-03-28 13:24:06 -0500

    Load from app.html to template each page.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateURL: 'app/app.html',
+    directives: [SongsComponent]
 })
 export class AppComponent { }
 
