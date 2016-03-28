489c45236733c4302ea6c38201c82f3083fe9cf7
Kyle <Kyle.rogers533@gmail.com>
2016-03-27 22:25:54 -0500

    Adds TS component constructor to pull the array of tracks from the firebase server

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,21 @@
 import {Component} from 'angular2/core';
+import {FirebaseService} from './lib/firebase';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }
+export class AppComponent { 
+    trackInfo: Array<any> = [];
+    isLoading: boolean = false;
+
+    constructor(private fs: FirebaseService) {
+        fs.dataRef.child('music').on('value', (snapshot: DataSnapshot) => {
+            if (snapshot) {
+                this.trackInfo = Object.keys(snapshot.val());
+            }
+        });
+    }
+
+}
 
